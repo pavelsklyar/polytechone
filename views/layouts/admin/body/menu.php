7fb791e161a1e601e9ac73b6fcb267839db5951f
Ivan Zaytsev <zaytsev.ivan.alekseevich@gmail.com>
2019-11-28 01:05:15 +0300

    a

---
+++ views/layouts/admin/body/menu.php
@@ -0,0 +1,14 @@
+<nav class="admin__nav">
+        <ul class="menu__admin">
+        
+        <li class="admin__item <?php if ($path->getPath()[1] === "") : ?>menu__item-select<?php endif; ?>">
+            <a href="/">none</a>
+        </li>
+        <li class="menu__item <?php if ($path->getPath()[1] === "") : ?>menu__item-select<?php endif; ?>">
+            <a href="/">none</a>
+        </li>
+        <li class="menu__item <?php if ($path->getPath()[1] === "") : ?>menu__item-select<?php endif; ?>">
+            <a href="/">none</a>
+        </li>
+    </ul>
+</nav>
