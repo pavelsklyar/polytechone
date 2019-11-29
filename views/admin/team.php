ca5753b0668ec1cede00e723b7a7a27e0d99f638
Ivan Zaytsev <zaytsev.ivan.alekseevich@gmail.com>
2019-11-29 04:19:52 +0300

    Team

--- views/admin/team.php
+++ views/admin/team.php
@@ -7,15 +7,15 @@
 $page->title = "Состав команды - Админ-панель Polytech:ONE";
 
 ?>
-
+<div class="admin__team">
 <table>
     <tr>
-        <td>Позиция</td>
-        <td>Имя и фамилия</td>
-        <td>Отдел</td>
-        <td>Должность</td>
-        <td>Фото</td>
-        <td>Редактировать</td>
+        <th>Позиция</th>
+        <th>Имя и фамилия</th>
+        <th>Отдел</th>
+        <th>Должность</th>
+        <th>Фото</th>
+        <th>Редактировать</th>
     </tr>
     <?php foreach ($team as $item) : ?>
         <tr>
@@ -24,7 +24,8 @@ $page->title = "Состав команды - Админ-панель Polytech:O
             <td><?= $item['department'] ?></td>
             <td><?= $item['position'] ?></td>
             <td><?= $item['photo'] ?></td>
-            <td>Иконка</td>
+            <td><img class="edit-icon" src="/images/edit.png"></td>
         </tr>
     <?php endforeach; ?>
 </table>
+</div>
